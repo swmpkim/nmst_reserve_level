979827c7e4e5348c10ffdecd3b7f9ac346f89dd2
Kim Cressman <kim@catbirdstats.com>
2023-04-21 10:39:49 -0500

    starting wrangling code

---
+++ R/functions.R
@@ -0,0 +1,37 @@
+# Importing ----
+
+get_data <- function(file){
+    readxl::read_xlsx(file,
+                          sheet = "Cover")
+}
+
+get_stn_table <- function(file){
+    readxl::read_xlsx(file,
+                          sheet = "Station_Table")
+}
+
+get_species_info <- function(file){
+    readxl::read_xlsx(file,
+                          sheet = "Species_Names")
+}
+
+
+
+# Summaries ----
+
+sum_category <- function(plant_cat,
+                         data = dat, 
+                         spp_data = species_info){
+    
+    cat_names <- spp_data$Species[which(spp_data$Plant_Categories == plant_cat)]
+    cat_cols <- which(names(data) %in% cat_names)
+    apply(data[,c(cat_cols)], 1, function(x) sum(x, na.rm = TRUE))
+}
+
+
+sum_all_categories <- function(data = dat, 
+                               spp_data = species_info){
+    # uses sum_category() from above
+    cats_to_summ <- unique(spp_data$Plant_Categories)
+    as.data.frame(sapply(cats_to_summ, sum_category, USE.NAMES = TRUE))
+}
