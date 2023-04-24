3e342da1da3c57a5492eaed8d68d6efacc6f84be
Kim Cressman <kim@catbirdstats.com>
2023-04-24 10:18:04 -0500

    functions for EIs; added parameter for reserve

--- R/functions.R
+++ R/functions.R
@@ -15,6 +15,28 @@ get_species_info <- function(file){
                           sheet = "Species_Names")
 }
 
+get_eis <- function(file){
+    read_xlsx(file,
+              sheet = "Ecotone_Invaders") %>% 
+        mutate(rownum = row_number()) %>% 
+        pivot_longer(-rownum,
+                     names_to = "Zone",
+                     values_to = "Invader") %>% 
+        filter(!is.na(Invader)) %>% 
+        select(-rownum) %>% 
+        arrange(Zone, Invader)
+}
+
+
+# Joins ----  
+
+join_zones <- function(data = dat,
+                       station_info = stn_tbl){
+    zones_to_join <- station_info[, c("Reserve", "SiteID", "TransectID", "PlotID", "Vegetation_Zone")]
+    
+    dplyr::left_join(data, zones_to_join) %>% 
+        relocate(Vegetation_Zone, .before = SiteID)
+}
 
 
 # Summaries ----
@@ -29,7 +51,7 @@ sum_category <- function(plant_cat,
 }
 
 
-sum_all_categories <- function(data = dat, 
+sum_spp_categories <- function(data = dat, 
                                spp_data = species_info){
     # uses sum_category() from above
     cats_to_summ <- unique(spp_data$Plant_Categories)
