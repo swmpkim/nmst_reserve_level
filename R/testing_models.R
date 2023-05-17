9ff57d72a3edb0e079dd7925e6b457e9f18a0a83
Kim Cressman <kim@catbirdstats.com>
2023-05-17 14:40:18 -0500

    more with emtrends

--- R/testing_models.R
+++ R/testing_models.R
@@ -1,5 +1,6 @@
 # load(here::here("data", "intermediate", "CBV_dfs.Rdata"))
 load(here::here("data", "intermediate", "GND_dfs.Rdata"))
+load(here::here("data", "intermediate", "NIW_dfs.Rdata"))
 
 library(lme4)
 library(tidyverse)
@@ -14,18 +15,20 @@ byGroup <- byGroup |>
                   Date_centered = Date - median(Date),
                   Years_sinceStart = Date - min(Date))
 
-mod <- lm(`H-Halophyte` ~ Date_centered*Vegetation_Zone,
+mod <- lm(`H-Halophyte` ~ Years_sinceStart*Vegetation_Zone,
           data = byGroup)
 anova(mod)
 summary(mod)
+emtrends(mod, pairwise ~ Vegetation_Zone, var = "Years_sinceStart")
+emmip(mod, Vegetation_Zone ~ Years_sinceStart, cov.reduce = range)
 
 
-mod2 <- lmer(`H-Halophyte` ~ Date_centered*Vegetation_Zone + (1|TransectID/PlotID),
+mod2 <- lmer(`H-Halophyte` ~ Years_sinceStart*Vegetation_Zone + (1|SiteID/TransectID/PlotID),
             data = byGroup)
 anova(mod2)
 summary(mod2)
-
-emmeans(mod2, "Vegetation_Zone")
+emtrends(mod2, pairwise ~ Vegetation_Zone, var = "Years_sinceStart")
+emmip(mod2, Vegetation_Zone ~ Years_sinceStart, cov.reduce = range)
 
 
 mod3 <- lm(EIR ~ Years_sinceStart*Vegetation_Zone,
@@ -47,19 +50,27 @@ emmip(mod3, Vegetation_Zone ~ Years_sinceStart, cov.reduce = range)
 # want to pull out slope by zone, from the model output
 # from research gate - remove the simple effect for the numerical variable to get all the slopes??
 # https://www.researchgate.net/post/How_can_I_get_confidence_intervals_for_multiple_slopes_in_R
-mod3b <- lm(EIR ~ Vegetation_Zone + Date:Vegetation_Zone,
+# this seems non-ideal but does seem to return the correct slopes (based on manually comparing slope for Low Marsh from summary(mod3))
+mod3b <- lm(EIR ~ Vegetation_Zone + Years_sinceStart:Vegetation_Zone,
             data = bySp)
 summary(mod3b)
 
 
+# Does all this work on mixed models???
 
-mod4 <- lmer(EIR ~ Date_centered*Vegetation_Zone + (1|TransectID/PlotID),
+mod4 <- lmer(EIR ~ Years_sinceStart*Vegetation_Zone + (1|SiteID/TransectID/PlotID),
              data = bySp)
 anova(mod4)
 summary(mod4)
 
-# need to do the following only if the interaction in the "big" model is significant
+emtrends(mod4, pairwise ~ Vegetation_Zone, var = "Years_sinceStart")
+# plot the predictions
+emmip(mod4, Vegetation_Zone ~ Years_sinceStart, cov.reduce = range)
 
+
+
+# need to do the following only if the interaction in the "big" model is significant
+# update - shouldn't need to do this at all, because of emmeans::emtrends
 by_zone <- function(data, response, method = "lm"){
     # response should be a character vector
     dat <- data
