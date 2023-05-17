024ad48b1d17aa7c11cb8563ae780f4d762be0eb
Kim Cressman <kim@catbirdstats.com>
2023-05-17 14:24:18 -0500

    simple effects/slopes

--- R/testing_models.R
+++ R/testing_models.R
@@ -3,13 +3,16 @@ load(here::here("data", "intermediate", "GND_dfs.Rdata"))
 
 library(lme4)
 library(tidyverse)
+library(emmeans)
 
 bySp <- bySp |> 
-    dplyr::mutate(Date_centered = Date - median(Date))
+    dplyr::mutate(Date_centered = Date - median(Date),
+                  Years_sinceStart = Date - min(Date))
 
 byGroup <- byGroup |> 
     dplyr::mutate(Date = lubridate::decimal_date(lubridate::ymd(paste(Year, Month, Day, sep = "-"))),
-                  Date_centered = Date - median(Date))
+                  Date_centered = Date - median(Date),
+                  Years_sinceStart = Date - min(Date))
 
 mod <- lm(`H-Halophyte` ~ Date_centered*Vegetation_Zone,
           data = byGroup)
@@ -22,13 +25,32 @@ mod2 <- lmer(`H-Halophyte` ~ Date_centered*Vegetation_Zone + (1|TransectID/PlotI
 anova(mod2)
 summary(mod2)
 
+emmeans(mod2, "Vegetation_Zone")
 
 
-
-mod3 <- lm(EIR ~ Date_centered*Vegetation_Zone,
+mod3 <- lm(EIR ~ Years_sinceStart*Vegetation_Zone,
           data = bySp)
 anova(mod3)
 summary(mod3)
+
+###### Pull out the groupwise slopes!!!!! 
+# simple effects was the term I needed
+# https://stats.oarc.ucla.edu/r/seminars/interactions-r/
+# https://cran.r-project.org/web/packages/emmeans/vignettes/interactions.html 
+
+# Also does pairwise contrasts - can see if slopes are different from each other, pairwise
+emtrends(mod3, pairwise ~ Vegetation_Zone, var = "Years_sinceStart")
+# plot the predictions
+emmip(mod3, Vegetation_Zone ~ Years_sinceStart, cov.reduce = range)
+
+
+# want to pull out slope by zone, from the model output
+# from research gate - remove the simple effect for the numerical variable to get all the slopes??
+# https://www.researchgate.net/post/How_can_I_get_confidence_intervals_for_multiple_slopes_in_R
+mod3b <- lm(EIR ~ Vegetation_Zone + Date:Vegetation_Zone,
+            data = bySp)
+summary(mod3b)
+
 
 
 mod4 <- lmer(EIR ~ Date_centered*Vegetation_Zone + (1|TransectID/PlotID),
@@ -36,7 +58,7 @@ mod4 <- lmer(EIR ~ Date_centered*Vegetation_Zone + (1|TransectID/PlotID),
 anova(mod4)
 summary(mod4)
 
-
+# need to do the following only if the interaction in the "big" model is significant
 
 by_zone <- function(data, response, method = "lm"){
     # response should be a character vector
@@ -45,6 +67,9 @@ by_zone <- function(data, response, method = "lm"){
     
     # resp <- response
     for(i in seq_along(zones)){
+        cat(zones[i])
+        cat('\n')
+        
         subs <- filter(dat, 
                        Vegetation_Zone == zones[i])
         
@@ -52,19 +77,23 @@ by_zone <- function(data, response, method = "lm"){
             formula <- eval(paste0("`", response, "` ~ Date_centered"))
             mod <- lm(formula,
                       data = subs)
+            print(summary(mod))
+            cat('\n\n')
         }
 
         if(method == "lmer"){
             formula <- eval(paste0("`", response, "` ~ Date_centered + (1|SiteID/TransectID/PlotID)"))
             mod <- lmer(formula,
                       data = subs)
+            print(anova(mod))
+            cat('\n\nCoefficients\n')
+            print(fixef(mod))
+            cat('\n\n\n')
         }
         
         
-        cat(zones[i])
-        cat('\n')
-        print(anova(mod))
-        cat('\n\n')
+        
+
     }
 }
 
