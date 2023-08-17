93e28320f817695a1570a6dfbd990402f0fb9bae
Kim Cressman <kim@catbirdstats.com>
2023-08-17 13:58:47 -0500

    don't get rid of empty cols in EIR table if those zones are in the station table

--- R/sourced/functions.R
+++ R/sourced/functions.R
@@ -40,9 +40,6 @@ get_species_info <- function(file){
 get_zone_ordering <- function(file){
     tmp <- read_xlsx(file,
               sheet = "Ecotone_Invaders")
-    # remove empty columns
-    empties <- which(colSums(!is.na(tmp)) == 0)
-    tmp <- tmp[-empties]
     # only keep zones that are in the station table
     tmp <- tmp[names(tmp) %in% unique(stn_tbl$Vegetation_Zone)]
     # put them in order
