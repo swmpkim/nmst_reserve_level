05db3400c767f550e3b38b906ab1f3f0565104ed
Kim Cressman <kim@catbirdstats.com>
2024-09-02 15:36:04 -0500

    work on shiny app

--- R/02_dataFormattingApp.R
+++ R/02_dataFormattingApp.R
@@ -36,10 +36,51 @@ ui <- fluidPage(
     h3("Namaste Veg File Exploration"),
     selectInput("file", "Which file would you like to use?", 
                 dir(here::here("data"))),
-    "Some key features of this file are:", 
-    verbatimTextOutput("summary"),
-    "And here is a preview:", 
-    tableOutput("preview")
+    
+    tabsetPanel(type = "tabs", id = "tabselected",
+                
+                tabPanel("1. Review data",
+                         value = 1,
+                         br(),
+                         "Some key features of this file are:",
+                         br(),
+                         verbatimTextOutput("summary"),
+                         br(),
+                         "And here is a preview:", 
+                         tableOutput("preview"),
+                         br()
+                         ),
+                
+                tabPanel("2. Choose options",
+                         value = 2,
+                         br(),
+                         selectInput("unvegspps", 
+                                     label = "What species should be included in 'Unvegetated'?",
+                                     choices = NULL,
+                                     multiple = TRUE),
+                         br(),
+                         selectInput("yrchoice", 
+                                     label = "What year(s) to work with?",
+                                     choices = NULL,
+                                     multiple = TRUE),
+                         br(),
+                         radioButtons("htdens", 
+                                      label = "Include Height and Density?",
+                                      choices = c("yes", "no"))),
+                
+                tabPanel("3. Preview CDMO layout",
+                         value = 3,
+                         h4("Preview of CDMO-shaped data"),
+                         tableOutput("long")),
+                
+                tabPanel("4. Download CDMO layout",
+                         value = 4,
+                         h4("Click the button below to download"),
+                         downloadButton("download_cdmo", 
+                                        label = "Download")
+                         )
+    
+    )
 )
 
 
@@ -49,9 +90,23 @@ server <- function(input, output, session) {
     # READ IN DATA -----
     dat <- reactive({
         req(input$file)
-        get_data(here::here("data", input$file), cover_only = TRUE)
+        get_data(here::here("data", input$file), 
+                 cover_only = ifelse(input$htdens == "yes",
+                                     FALSE,
+                                     TRUE))
     })
     
+    # Observe when the data frame changes and update the selectInput choices
+    observe({
+        req(dat())  
+        dat_yrs <- unique(dat()$Year)  # Extract unique categories
+        updateSelectInput(session, "yrchoice", choices = dat_yrs)
+        
+        dat_spps <- names(dat())[(which(names(dat()) == "Total") + 1):20]
+        updateSelectInput(session, "unvegspps", choices = dat_spps)
+    })
+    
+    # GENERATE PREVIEW FOR SUMMARY
     dat_summ <- reactive({
         out <- list("Sites" = unique(dat()$SiteID),
                     "First Few Species" = names(dat())[(which(names(dat()) == "Total") + 1):20],
@@ -59,10 +114,24 @@ server <- function(input, output, session) {
         out
     })
     
+    # PIVOT DATA
+    dat_cdmo_measurements <- reactive({
+        pivot_to_cdmo(dat())
+    })
     
     # OUTPUTS  
     output$preview <- renderTable(dat()[1:10, 1:10])
     output$summary <- renderPrint(dat_summ())
+    output$long <- renderTable(dat_cdmo_measurements()[1:10, ])
+    output$download_cdmo <- downloadHandler(
+        filename = function() {
+            paste0("download ", as.character(Sys.time()), ".xlsx")
+        },
+        content = function(file) {
+            writexl::write_xlsx(dat_cdmo_measurements(),
+                                file)
+        }
+    )
 }
 
 # Call app ----
